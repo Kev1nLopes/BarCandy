c7cf35aba7ea716e68100fa3c153300d6368867e
Kev1nLopes <ketisinho14@gmail.com>
2021-08-29 21:38:19 -0300

    trying to increment menu animation for mobile devices

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,25 @@
 (function(){
     let btnnav = document.getElementById("btnnav");
-    console.log(btnnav);
+    let menu = document.getElementById("menu");
+    let lis = document.querySelectorAll("#menu li")
+    let open = false;
+    console.log(lis);
+
+    btnnav.addEventListener("click", function(){
+        open = !open;
+        if(open == false){
+            menu.style.width= "100px";
+            lis.forEach((li)=>{
+                li.style.display = "block"
+            })
+        }else{
+            menu.style.width= "0px";
+            lis.forEach((li)=>{
+                li.style.display = "none"
+            })
+        }
+        
+    })
+    
+
 })();
