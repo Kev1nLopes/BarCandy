a1c190a42a97e6886b0beca20d3740824a3acb71
Kev1nLopes <ketisinho14@gmail.com>
2021-09-01 14:00:20 -0300

    now i think that i fix the bug that crash my brain

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
 (function(){
-    alert("Oi")
     let menu = document.getElementById("menu");
     let list = document.getElementById("list");
+    console.log(list)
    
     menu.addEventListener("click", function(){
+        let h = false
         if(list.classList.contains("list-open")){
-            list.classList.remove("list-open")
+            list.classList.remove("list-open");
+          
         }else{
             list.classList.add("list-open");
-            document.querySelectorAll(".nav-list li").forEach((li)=>{
-                li.style.display = "block";
-            })
+           
         }
-    })
+    });
    
 })();
 
